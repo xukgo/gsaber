24b582542416749a4d78e671e57ef9f075a2f914
xukuan <xukuan@wxchina.com>
2020-03-20 17:08:01 +0800

    start的时候就更新一次

--- compon/fileWatcher/timer.go
+++ compon/fileWatcher/timer.go
@@ -11,15 +11,13 @@ var localWatchers = make([]*Watcher, 0, 0)
 
 func Start() {
 	fmt.Printf("start file watch service\r\n")
+	update()
+
 	minInterval := 200
 	go func() {
 		for {
-			localWatcherLocker.RLock()
-			for idx := range localWatchers {
-				localWatchers[idx].do()
-			}
-			localWatcherLocker.RUnlock()
 			time.Sleep(time.Duration(minInterval) * time.Millisecond)
+			update()
 		}
 	}()
 }
@@ -28,4 +26,12 @@ func AddWatcher(model *Watcher) {
 	localWatcherLocker.Lock()
 	localWatchers = append(localWatchers, model)
 	localWatcherLocker.Unlock()
+}
+
+func update() {
+	localWatcherLocker.RLock()
+	for idx := range localWatchers {
+		localWatchers[idx].do()
+	}
+	localWatcherLocker.RUnlock()
 }
