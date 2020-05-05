0824e648aa38570e3bc072b4477f87b66fb4a8e5
xukuan <xukuan@wxchina.com>
2020-05-05 23:20:38 +0800

    添加获取穿透软连接到当前运行时路径

--- utils/fileUtil/pathUtil_linux.go
+++ utils/fileUtil/pathUtil_linux.go
@@ -10,14 +10,42 @@ import (
 )
 
 /*
-获取程序运行路径
+获取程序运行路径，不会穿透软连接
 */
 func GetCurrentDirectory() string {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	var dir string
+	ex, err := os.Executable()
 	if err != nil {
 		fmt.Println(err)
 		return ""
 	}
+
+	dir = filepath.Dir(ex)
+	//dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return ""
+	//}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+
+/*
+获取程序运行路径，穿透软连接
+*/
+func GetRealRunDirectory() string {
+	var dir string
+	ex, err := os.Executable()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	exReal, err := filepath.EvalSymlinks(ex)
+	if err != nil {
+		panic(err)
+	}
+	dir = filepath.Dir(exReal)
+	//fmt.Println(dir)
 	return strings.Replace(dir, "\\", "/", -1)
 }
 
