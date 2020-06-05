75e38ba8e3c657b65e9c9e31dc1fd654a4a5899b
xukuan <xukuan@wxchina.com>
2020-06-05 16:46:04 +0800

    添加自动识别时间戳位数并且转换程ns的时间戳的方法

--- utils/timeUtil/timeUtil.go
+++ utils/timeUtil/timeUtil.go
@@ -1,10 +1,12 @@
 package timeUtil
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
 
-const CN_LOCATION ="Asia/Chongqing"
+const CN_LOCATION = "Asia/Chongqing"
 
 func ParseWithCnLocation(layout string, timeStr string) (time.Time, error) {
 	if l, err := time.LoadLocation(CN_LOCATION); err != nil {
@@ -14,4 +16,26 @@ func ParseWithCnLocation(layout string, timeStr string) (time.Time, error) {
 		lt, err := time.ParseInLocation(layout, timeStr, l)
 		return lt, err
 	}
+}
+
+func ParseTsToUnixNano(ts string) (int64, error) {
+	tslen := len(ts)
+	var factor int64 = 0
+	//秒：1591343262 长度10
+	if tslen == 9 || tslen == 10 || tslen == 11 {
+		factor = int64(time.Second)
+	} else if tslen == 12 || tslen == 13 || tslen == 14 { //ms
+		factor = int64(time.Millisecond)
+	} else if tslen == 15 || tslen == 16 || tslen == 17 { //us
+		factor = int64(time.Microsecond)
+	} else if tslen == 18 || tslen == 19 || tslen == 20 { //ns
+		factor = int64(time.Nanosecond)
+	} else {
+		return 0, fmt.Errorf("wrong timestamp value")
+	}
+	v, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return v * factor, nil
 }
