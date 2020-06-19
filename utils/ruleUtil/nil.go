e7fef7069d79a5ae39d33a9e17b25b612526aab9
xukuan <xukuan@wxchina.com>
2020-06-19 10:34:40 +0800

    添加interface是否是nil的判断

---
+++ utils/ruleUtil/nil.go
@@ -0,0 +1,11 @@
+package ruleUtil
+
+import "reflect"
+
+func InterfaceIsNil(i interface{}) bool {
+	vi := reflect.ValueOf(i)
+	if vi.Kind() == reflect.Ptr {
+		return vi.IsNil()
+	}
+	return false
+}
