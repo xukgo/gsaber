cc00331eb51dcdd95b4ff930693e3600f0b1bdbb
xukuan <xukuan@casachina.com.cn>
2024-04-04 17:33:03 +0800

    add generic function

--- utils/arrayUtil/array_generic.go
+++ utils/arrayUtil/array_generic.go
@@ -1,5 +1,16 @@
 package arrayUtil
 
+// IndexOfArray 返回切片中第一次出现目标值的索引位置。
+// 如果找不到，则返回-1。
+// 这是一个泛型函数，支持任何可比较的类型。
+func IndexOfArray[T comparable](s []T, target T) int {
+	for i, v := range s {
+		if v == target {
+			return i
+		}
+	}
+	return -1
+}
 func SplitIntoChunks[T any](s []T, chunkSize int) [][]T {
 	var chunks [][]T
 	for chunkSize < len(s) {
@@ -7,6 +18,23 @@ func SplitIntoChunks[T any](s []T, chunkSize int) [][]T {
 	}
 	chunks = append(chunks, s)
 	return chunks
+}
+
+func SpreadNilToCountList[T any](items []T, count int) []T {
+	if len(items) == 0 {
+		return make([]T, 0)
+	}
+
+	minCount := len(items)
+	if len(items) > count {
+		minCount = count
+	}
+
+	res := make([]T, count)
+	for i := 0; i < minCount; i++ {
+		res[i] = items[i]
+	}
+	return res
 }
 
 func SpreadLoopToCountList[T any](items []T, count int) []T {
